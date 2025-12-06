d9c2d9ec787a76126fd93fd3a2c410d5f5b17720
Naurla <josenolangamaliel123@gmail.com>
2025-12-06 22:07:00 +0800

    settings

--- app/src/main/java/com/example/scheduleconnect/LanguageFragment.kt
+++ app/src/main/java/com/example/scheduleconnect/LanguageFragment.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
 class LanguageFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_language, container, false)
+        val view = inflater.inflate(R.layout.fragment_language, container, false)
+
+        // Handle Back Button Click
+        val btnBack = view.findViewById<ImageView>(R.id.btnBackLanguage)
+        btnBack.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+
+        return view
     }
 }
--- app/src/main/java/com/example/scheduleconnect/NotificationsFragment.kt
+++ app/src/main/java/com/example/scheduleconnect/NotificationsFragment.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
 class NotificationsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // This line "inflates" (loads) the XML design you created earlier
-        return inflater.inflate(R.layout.fragment_notifications_settings, container, false)
+        // Inflate the layout
+        val view = inflater.inflate(R.layout.fragment_notifications_settings, container, false)
+
+        // Handle Back Button Click
+        val btnBack = view.findViewById<ImageView>(R.id.btnBackNotif)
+        btnBack.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+
+        return view
     }
 }
